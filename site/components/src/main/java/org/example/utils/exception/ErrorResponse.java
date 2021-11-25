5077b846d45508c0cf8ea0d47c79dd1297b2524e
kumarceg <kumarceg@gmail.com>
2021-11-25 15:30:42 +0100

    custom exception added

--- site/components/src/main/java/org/example/utils/exception/ErrorResponse.java
+++ site/components/src/main/java/org/example/utils/exception/ErrorResponse.java
@@ -5,6 +5,22 @@ package org.example.utils.exception;
  */
 public class ErrorResponse {
 
+    public int get_error_code() {
+        return _error_code;
+    }
+
+    public void set_error_code(int _error_code) {
+        this._error_code = _error_code;
+    }
+
+    public String get_error_desc() {
+        return _error_desc;
+    }
+
+    public void set_error_desc(String _error_desc) {
+        this._error_desc = _error_desc;
+    }
+
     private int _error_code;
     private String _error_desc;
 
--- site/components/src/main/java/org/example/utils/exception/NodeExceptionHandler.java
+++ site/components/src/main/java/org/example/utils/exception/NodeExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.example.utils.exception;
 
+import javax.jcr.PathNotFoundException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -13,14 +14,19 @@ public class NodeExceptionHandler implements ExceptionMapper<Exception> {
     @Override
     public Response toResponse(Exception exception) {
         Response response = null;
-        ErrorResponse errorResponse = null;
+        ErrorResponse errorResponse;
         if (exception instanceof NonUniqueException) {
             errorResponse = new ErrorResponse(Response.Status.CONFLICT.getStatusCode(), exception.getMessage());
             response = Response.status(Response.Status.CONFLICT.getStatusCode()).entity(errorResponse).type(MediaType.APPLICATION_JSON).build();
-        } else if (exception instanceof Exception) {
+        } else if (exception instanceof PathNotFoundException) {
+            errorResponse = new ErrorResponse(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), PATH_NOT_FOUND + exception.getMessage());
+            response = Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(errorResponse).type(MediaType.APPLICATION_JSON).build();
+        } else if (exception != null) {
             errorResponse = new ErrorResponse(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), exception.getMessage());
             response = Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(errorResponse).type(MediaType.APPLICATION_JSON).build();
         }
         return response;
     }
+
+    private static final String PATH_NOT_FOUND = "The provided path is Invalid, path=";
 }
